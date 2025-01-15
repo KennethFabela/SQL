55119fdfb6533dfaed962fcd698ef276c86a444a
Kenneth Fabela <158118155+KennethFabela@users.noreply.github.com>
2025-01-14 17:27:14 -0700

    Updated exploratory_data_analysis.sql
    Added more code comments

--- SQL_exploratory_data_analysis_project/exploratory_data_analysis.sql
+++ SQL_exploratory_data_analysis_project/exploratory_data_analysis.sql
@@ -3,37 +3,44 @@
 SELECT *
 FROM layoffs_staging2;
 
-
-SELECT MAX(total_laid_off), MAX(percentage_laid_off)
-FROM layoffs_staging2;
-
-SELECT *
-FROM layoffs_staging2
-WHERE percentage_laid_off = 1
-ORDER BY funds_raised_millions DESC;
-
-SELECT company, SUM(total_laid_off)
-FROM layoffs_staging2
-GROUP BY company
-ORDER BY 2 DESC;
-
 -- Date Range
 SELECT MIN(`date`), MAX(`date`)
 FROM layoffs_staging2;
 
-SELECT country, SUM(total_laid_off)
-FROM layoffs_staging2
-GROUP BY country
-ORDER BY 2 DESC;
-
-SELECT *
-FROM layoffs_staging2;
-
+-- Total employees laid off by year
 SELECT YEAR(`date`), SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY YEAR(`date`)
 ORDER BY 1 DESC;
 
+-- Shows the top 5 companies that laid off the most employees at once
+SELECT *
+FROM layoffs_staging2
+ORDER BY total_laid_off DESC
+LIMIT 5;
+
+-- Top 5 companies that raised the most funds total throughout the entire date range
+SELECT company, MAX(funds_raised_millions) AS max_funds
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY max_funds DESC
+LIMIT 5;
+
+-- Shows the top 10 companies that laid off the most employees total throughout the entire 
+-- date range
+SELECT company, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC
+LIMIT 10;
+
+-- Shows the top 5 countries that laid off the most employees total throughout the entire
+-- date range
+SELECT country, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY country
+ORDER BY 2 DESC
+LIMIT 5;
 
 SELECT stage, SUM(total_laid_off)
 FROM layoffs_staging2
@@ -53,6 +60,14 @@ WHERE SUBSTRING(`date`, 1, 7) IS NOT NULL
 GROUP BY `Month`
 ORDER BY 1 ASC;
 
+-- Top 5 months with most employees laid off
+SELECT SUBSTRING(`date`, 1, 7) AS `Month`, SUM(total_laid_off)
+FROM layoffs_staging2
+WHERE SUBSTRING(`date`, 1, 7) IS NOT NULL
+GROUP BY `Month`
+ORDER BY 2 DESC
+LIMIT 5;
+
 -- Rolling total of layoffs
 WITH Rolling_Total AS
 (
@@ -65,14 +80,6 @@ ORDER BY 1 ASC
 SELECT `Month`, total_off
 , SUM(total_off) OVER(ORDER BY `Month`) AS rolling_total
 FROM Rolling_Total;
-
-
-
-
-SELECT company, SUM(total_laid_off)
-FROM layoffs_staging2
-GROUP BY company
-ORDER BY 2 DESC;
 
 SELECT company, YEAR(`date`), SUM(total_laid_off)
 FROM layoffs_staging2
